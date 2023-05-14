ae8d4454a34ca1c28d280da6305737afc8e5b818
Larry H <111525408+larryh12@users.noreply.github.com>
2023-05-14 16:41:35 +0700

    add rosepine palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,25 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'rp-base': '#191724',
+        'rp-surface': '#1f1d2e',
+        'rp-overlay': '#26233a',
+        'rp-muted': '#6e6a86',
+        'rp-subtle': '#908caa',
+        'rp-text': '#e0def4',
+        'rp-love': '#eb6f92',
+        'rp-gold': '#f6c177',
+        'rp-rose': '#ebbcba',
+        'rp-pine': '#31748f',
+        'rp-foam': '#9ccfd8',
+        'rp-iris': '#c4a7e7',
+        'rp-highlow': '#21202e',
+        'rp-highmed': '#403d52',
+        'rp-highhigh': '#524f67',
+      },
+    },
   },
   plugins: [],
 };
