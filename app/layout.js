6ae1a8635dba6635a24baede4766b23cc7b0a45d
Larry H <111525408+larryh12@users.noreply.github.com>
2023-05-17 02:16:02 +0700

    refactor layout code

--- app/layout.js
+++ app/layout.js
@@ -6,20 +6,17 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'LARRY HUYNH',
-  description:
-    "Larry Huynh's Personal Website | Get in touch: larry.huynh.dev@outlook.com",
+  description: "Larry Huynh's Personal Website",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${inter.className} flex items-center justify-center bg-rp-base text-rp-text`}
-      >
-        <div className="w-full max-w-screen-lg p-8">
+      <body className="flex justify-center bg-rp-base text-rp-text">
+        <main className={`${inter.className} max-w-screen-lg p-8`}>
           <Navbar />
           {children}
-        </div>
+        </main>
       </body>
     </html>
   );
