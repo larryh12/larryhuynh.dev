1048dfbc57fc5d48d6012eff5222cc98b4f0ff7d
Larry H <111525408+larryh12@users.noreply.github.com>
2023-05-17 15:07:59 +0700

    refactor home section

--- components/Hero.js
+++ components/Hero.js
@@ -9,42 +9,53 @@ const Hero = () => {
   return (
     <section
       id="home"
-      className="flex flex-col-reverse py-8 md:flex-row md:py-48"
+      className="flex min-h-screen flex-col-reverse md:flex-row"
     >
-      <div className="flex-1">
-        <div className="my-0.5 text-rp-subtle md:my-1">Hi, my name is</div>
-        <div className="my-0.5 inline-block bg-gradient-to-r from-rp-rose to-rp-love bg-clip-text text-7xl font-bold text-transparent md:my-1">
-          LARRY
-        </div>
-        <div />
-        <div className="my-0.5 inline-block bg-gradient-to-r from-rp-foam to-rp-pine bg-clip-text text-7xl font-bold text-transparent md:my-1">
-          HUYNH
-        </div>
-        <div className="my-0.5 text-rp-subtle md:my-1">I am a</div>
-        <div className="my-0.5 text-lg font-semibold md:my-1">
-          Software Engineering Graduate
-        </div>
-        <div className="mt-4 flex flex-row text-sm text-rp-text md:mt-8">
-          <a href="#about">
-            <div className="mr-6 flex flex-row rounded-lg bg-rp-overlay p-2 pr-4 hover:bg-rp-highmed">
-              <AiOutlineInfoCircle className="mr-1 pt-1 text-xl" />
-              About me
+      <div
+        id="home__text__container"
+        className="flex flex-1 items-start justify-center md:items-center"
+      >
+        <div id="home__text__wrapper" className="space-y-8">
+          <div id="home__text__upper" className="space-y-0.5">
+            <div className="text-rp-subtle">Hi, my name is</div>
+            <div className="inline-block bg-gradient-to-r from-rp-rose to-rp-love bg-clip-text text-7xl font-bold text-transparent">
+              LARRY
             </div>
-          </a>
-          <a
-            href="https://www.linkedin.com/in/larry-huynh-dev/"
-            target="_blank"
+            <div />
+            <div className="inline-block bg-gradient-to-r from-rp-foam to-rp-pine bg-clip-text text-7xl font-bold text-transparent">
+              HUYNH
+            </div>
+            <div className="text-rp-subtle">I am a</div>
+            <div className="text-lg font-semibold">
+              Software Engineering Graduate
+            </div>
+          </div>
+          <div
+            id="home__text__lower"
+            className="flex space-x-4 text-sm text-rp-text [&>a]:flex [&>a]:items-center [&>a]:space-x-1 [&>a]:rounded-lg [&>a]:bg-rp-overlay [&>a]:px-3 [&>a]:py-2 hover:[&>a]:bg-rp-highmed"
           >
-            <div className="flex flex-row rounded-lg bg-rp-overlay p-2 pr-4 hover:bg-rp-highmed">
-              <AiOutlineDownload className="mr-1 pt-1 text-xl" />
-              Résumé/CV
-            </div>
-          </a>
+            <a href="#about">
+              <AiOutlineInfoCircle />
+              <span>About me</span>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/larry-huynh-dev/"
+              target="_blank"
+            >
+              <AiOutlineDownload />
+              <span>Résumé/CV</span>
+            </a>
+          </div>
         </div>
       </div>
-      <div className="mb-12 flex flex-1 items-center justify-center md:justify-end">
-        <div className="blob max-w-[67%] overflow-hidden border border-rp-text bg-rp-rose">
-          <Image src={avatar} alt="avatar" />
+      <div
+        id="home__avatar__container"
+        className="flex flex-1 items-center justify-center"
+      >
+        <div id="home__avatar__wrapper" className="max-w-[66%]">
+          <div className="home__avatar__blob overflow-hidden border border-rp-text bg-rp-rose">
+            <Image src={avatar} alt="avatar" />
+          </div>
         </div>
       </div>
     </section>
