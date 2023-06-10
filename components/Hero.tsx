9ea51b453ed5887c41906db787ff4e4c3de45d6e
Larry Huynh <111525408+larryh12@users.noreply.github.com>
2023-06-11 03:24:31 +0700

    Add vercel hero animation

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -14,26 +14,31 @@ const Hero = (props: any) => {
         <div className="flex-1">
           <div className="prose mx-auto w-fit lg:mx-0">
             <p>Hi, my name is</p>
-            <h1 className="my-0 w-fit bg-gradient-to-r from-primary-focus to-primary bg-clip-text text-7xl uppercase text-transparent sm:text-9xl">
+            <h1 className="animate-vercel1 my-0 w-fit bg-clip-text text-7xl font-black uppercase text-transparent sm:text-9xl">
               {name.first}
             </h1>
-            <h1 className="my-0 w-fit bg-gradient-to-r from-secondary-focus to-secondary bg-clip-text text-7xl uppercase text-transparent sm:text-9xl">
+            <h1 className="animate-vercel2 my-0 w-fit bg-clip-text text-7xl font-black uppercase text-transparent sm:text-9xl">
               {name.last}
             </h1>
             <p>I am a</p>
-            <h2 className="my-0 text-lg sm:text-3xl">{name.title}</h2>
+            <h2 className="animate-vercel3 my-0 w-fit bg-clip-text text-lg text-transparent sm:text-3xl">
+              {name.title}
+            </h2>
           </div>
         </div>
         <div className="flex flex-1 justify-center lg:justify-end">
           <div className="flex flex-col justify-between">
-            <Image
-              src={name.avatar}
-              width={512}
-              height={512}
-              priority={true}
-              alt="avatar"
-              className="animate-blob aspect-square w-48 overflow-hidden bg-gradient-to-r from-primary to-secondary sm:w-72"
-            />
+            <div className="animate-blob overflow-hidden">
+              <Image
+                src={name.avatar}
+                width={512}
+                height={512}
+                priority={true}
+                alt="avatar"
+                className="animate-vercelfull aspect-square w-48 sm:w-72"
+              />
+            </div>
+
             <HeroBtns />
           </div>
         </div>
