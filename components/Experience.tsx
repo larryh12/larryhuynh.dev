9017b6f8b6afdef9e722d2d0548e2aac68c081f1
Larry Huynh <111525408+larryh12@users.noreply.github.com>
2023-06-12 03:04:36 +0700

    Complete Exp section rework

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -5,6 +5,7 @@ import SectionHead from './SectionHead';
 
 const Experience = (props: any) => {
   const exp = props.exp;
+  const projs = props.projs;
 
   return (
     <section
@@ -12,112 +13,12 @@ const Experience = (props: any) => {
       className="flex w-full items-center justify-center bg-base-100 px-4 py-20 sm:px-8 lg:min-h-screen"
     >
       <div className="flex w-full max-w-screen-lg flex-col">
-        {/* <div className="experience__heading text-center">
-
-          <div className="experience__main pt-8">
-            <div className="experience__main__caeepr space-y-8 text-left">
-              <div className="emc__header flex gap-4">
-                <div className="m-auto">
-                  <Image
-                    src={griffith}
-                    width={64}
-                    height={64}
-                    alt="griffith-logo"
-                    className=""
-                  />
-                </div>
-
-                <ul className="flex-1">
-                  <li className="text-xl font-semibold">
-                    Software Developer @
-                  </li>
-                  <li>
-                    <a
-                      href="https://www.griffith.edu.au/griffith-business-school/research/centre-applied-energy-economics-policy-research"
-                      target="_blank"
-                      className="flex items-center gap-1 text-lg font-medium"
-                    >
-                      CAEEPR <HiExternalLink />
-                    </a>
-                  </li>
-                  <li className="font-medium text-accent">
-                    07/2022 - 11/2022 | Internship
-                  </li>
-                </ul>
-              </div>
-
-              <ul className="emc__content space-y-4">
-                <li>
-                  • Developed an efficient and effective data extraction tool
-                  using Python frameworks for a large-scale data problem in the
-                  national electricity market research by AEMO and CAEEPR.
-                </li>
-                <li>
-                  • Enhanced the staff efficiency and productivity by automating
-                  the data extraction process, saving up to 80% of time and
-                  resources compared to manual methods.
-                </li>
-                <li>
-                  • Improved the data readability and accuracy by 2x by
-                  filtering and separating data by type and device identifiers.
-                </li>
-                <li>
-                  • Reduced the data size by 96% by removing duplicated and
-                  irrelevant entries, saving up to 70 MB of disk storage per
-                  file.
-                </li>
-                <li>
-                  • Provided valuable insights for future research on generator
-                  bidding strategies, allowing the staff to analyse and compare
-                  different scenarios and outcomes, improving the overall
-                  research quality by 3x.
-                </li>
-                <li>
-                  • Received Finalist Place for The Best Innovative Project in
-                  Trimester 2, 2022 out of 50 competing projects for delivering
-                  a user-friendly application that met the client&apos;s
-                  objectives and expectations.
-                </li>
-              </ul>
-
-              <div className="card card-normal static bg-neutral shadow-2xl sm:card-side">
-                <figure>
-                  <a
-                    target="_blank"
-                    href="https://github.com/larryh12/aemo-tool"
-                  >
-                    <Image
-                      src={aemo}
-                      alt="aemo"
-                      className="h-full object-cover transition hover:scale-105"
-                    />
-                  </a>
-                </figure>
-                <div className="card-body">
-                  <h2 className="card-title">AEMO Data Tool</h2>
-                  <p>
-                    Australian Energy Market Operator Data Tool - Interactive
-                    GUI application that provides data processing and data
-                    extraction features for AEMO raw data source.
-                  </p>
-                  <ul className="card-actions mt-2">
-                    <li className="badge badge-outline">Python</li>
-                    <li className="badge badge-outline">pandas</li>
-                    <li className="badge badge-outline">NumPy</li>
-                    <li className="badge badge-outline">openpyxl</li>
-                    <li className="badge badge-outline">PySimpleGUI</li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div> */}
         <SectionHead title="Experience" />
         <div className="flex flex-col">
           {exp.map((entry: any, index: number) => (
-            <div key={index} className="">
-              <div className="flex flex-wrap-reverse justify-between">
-                <div className="prose">
+            <div key={index} className="prose max-w-none">
+              <div className="flex flex-col-reverse sm:flex-row sm:justify-between">
+                <div>
                   <h2>{entry.head.title}</h2>
                   <h3 className="w-fit">
                     <a href={entry.head.link} target="_blank">
@@ -131,16 +32,51 @@ const Experience = (props: any) => {
                   alt={entry.head.name}
                   width={128}
                   height={128}
-                  className="object-contain p-2"
+                  className="mask mask-squircle my-0 object-contain"
                 />
               </div>
-              <div className="prose">
-                <ul>
-                  {entry.body.map((item: string, index: number) => (
-                    <li key={index}>{item}</li>
-                  ))}
-                </ul>
-              </div>
+              <ul>
+                {entry.body.map((item: string, index: number) => (
+                  <li key={index}>{item}</li>
+                ))}
+              </ul>
+              {projs
+                .filter((proj: any) => entry.work.includes(proj.name))
+                .map((proj: any) => (
+                  <a
+                    key={proj.name}
+                    href={proj.homepage === '' ? proj.html_url : proj.homepage}
+                    aria-label={proj.name}
+                    target="_blank"
+                    className="not-prose card card-compact no-underline transition sm:card-side sm:card-normal hover:bg-neutral"
+                  >
+                    <figure>
+                      <Image
+                        src={`https://raw.githubusercontent.com/larryh12/${proj.name}/main/docs/_og.png`}
+                        alt={proj.name}
+                        width={640}
+                        height={320}
+                        className="h-full rounded-2xl object-cover"
+                      />
+                    </figure>
+                    <div className="card-body text-base-content/80">
+                      <h2 className="card-title text-base-content/100">
+                        {proj.name}
+                      </h2>
+                      <p>{proj.description}</p>
+                      <ul className="card-actions mt-2">
+                        {proj.topics.map((topic: string) => (
+                          <li
+                            key={topic}
+                            className="badge badge-outline badge-sm"
+                          >
+                            {topic}
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                  </a>
+                ))}
             </div>
           ))}
         </div>
