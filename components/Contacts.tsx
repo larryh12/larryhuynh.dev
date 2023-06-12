26e3ad3bb7387630889e384b34134fa5a5b2d8d8
Larry Huynh <111525408+larryh12@users.noreply.github.com>
2023-06-13 04:10:32 +0700

    Complete Contacts comp rework

--- components/Contacts.tsx
+++ components/Contacts.tsx
@@ -2,86 +2,53 @@ import React from 'react';
 import Image from 'next/image';
 
 import { FaUser, FaEnvelope, FaLinkedin, FaGithub } from 'react-icons/fa';
-import { IoFlowerOutline, IoLogoVercel } from 'react-icons/io5';
 
 const Contacts = (props: any) => {
-  return (
-    <section id="contacts" className="flex px-8 py-16 sm:min-h-screen">
-      {/* <div className="contacts__wrapper m-auto flex flex-col">
-        <div className="contacts__heading text-center">
-          <div className="inline-block bg-gradient-to-r from-primary to-secondary bg-clip-text text-center text-2xl font-bold text-transparent">
-            <p>Contacts</p>
-            <div className="h-0.5 bg-gradient-to-r from-primary to-secondary" />
-          </div>
-        </div>
+  const links = props.links;
 
-        <div className="contacts__main w-full space-y-8 pt-8">
-          <p className="text-center text-xl font-semibold">
-            🤙 Get in touch with me!
-          </p>
-          <div className="card card-normal static bg-neutral shadow-2xl sm:card-side">
-            <figure>
+  return (
+    <section
+      id="contacts"
+      className="flex min-h-screen w-full items-center justify-center bg-base-100 px-4 py-20 sm:px-8"
+    >
+      <div className="prose w-full max-w-screen-lg">
+        <h1 className="mx-auto my-0 w-fit bg-gradient-to-r from-primary to-secondary bg-clip-text text-transparent">
+          Contacts
+        </h1>
+        <div className="flex flex-col items-center">
+          <h2 className="mt-20">🤙 Get in touch with me!</h2>
+          <div className="not-prose mt-10 flex flex-col gap-10 rounded-2xl bg-neutral px-10 py-20 text-neutral-content drop-shadow-lg sm:flex-row">
+            <div className="animate-vercelfull mx-auto h-fit w-fit flex-1 overflow-hidden rounded-xl p-0.5">
               <Image
-                src={avatar}
+                src={links.name.avatar}
                 alt="avatar"
-                className="m-8 mb-0 w-52 rounded-xl bg-base-200 sm:mb-8 sm:mr-0"
+                width={256}
+                height={256}
+                className="rounded-xl bg-neutral"
               />
-            </figure>
-            <div className="card-body justify-center [&>a]:flex [&>a]:items-center [&>a]:gap-2 [&>a]:border-b [&>a]:border-neutral-content [&>a]:py-2 [&>a]:font-medium sm:[&>a]:text-lg sm:[&>a]:font-semibold">
-              <a href="#">
-                <FaUser /> Larry Huynh
+            </div>
+            <div className="grid flex-2 gap-4 text-lg font-medium [&>*]:flex [&>*]:items-center [&>*]:gap-4 [&>*]:border-b [&>*]:border-neutral-content [&>*]:pb-1">
+              <span>
+                <FaUser /> {links.name.first} {links.name.last}
+              </span>
+              <a href={links.email.link}>
+                <FaEnvelope /> {links.email.id}
               </a>
-              <a href="mailto:larry.huynh.dev@outlook.com">
-                <FaEnvelope /> larry.huynh.dev@outlook.com
+              <a href={links.linkedin.link}>
+                <FaLinkedin />
+                {links.linkedin.id}
               </a>
-              <a
-                target="_blank"
-                href="https://www.linkedin.com/in/larry-huynh-dev/"
-              >
-                <FaLinkedin /> larry-huynh-dev
-              </a>
-              <a target="_blank" href="https://github.com/larryh12">
-                <FaGithub /> larryh12
+              <a href={links.github.link}>
+                <FaGithub />
+                {links.github.id}
               </a>
             </div>
           </div>
         </div>
-
-        <div className="mt-32 w-full space-y-4">
-          <p className="prose mx-auto text-center text-xs">
-            Copyright © 2023 larryhuynh.dev
-          </p>
-          <a
-            href="https://forthebadge.com/"
-            target="_blank"
-            aria-label="forthebadge"
-            className="mx-auto flex w-fit items-center bg-accent [&>p]:px-4 [&>p]:py-2 [&>p]:font-bold [&>p]:text-base-100"
-          >
-            <p className="text-sm tracking-widest">BUILT WITH</p>
-            <p className="bg-accent-focus text-xl">♥</p>
-          </a>
-          <div className="mx-auto flex w-fit gap-4 font-bold text-white [&>a]:flex [&>a]:items-center [&>a]:gap-1 [&>a]:px-4 [&>a]:py-1.5">
-            <a
-              href="https://daisyui.com/"
-              target="_blank"
-              aria-label="daisyui"
-              className="bg-[#5A0EF8]"
-            >
-              <IoFlowerOutline className="text-lg" />
-              <p>daisyUI</p>
-            </a>
-            <a
-              href="https://vercel.com/"
-              target="_blank"
-              aria-label="vercel"
-              className="bg-black tracking-wider"
-            >
-              <IoLogoVercel className="text-lg" />
-              <p>Vercel</p>
-            </a>
-          </div>
-        </div>
-      </div> */}
+        <p className="absolute bottom-5 w-full max-w-screen-lg text-center text-xs">
+          Built with ❤️ © 2023 larryhuynh.dev
+        </p>
+      </div>
     </section>
   );
 };
