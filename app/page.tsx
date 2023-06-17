6d2784f3506908d5c949344d7c345b5072682d64
Larry Huynh <111525408+larryh12@users.noreply.github.com>
2023-06-18 01:18:51 +0700

    fix: remove route revalidate

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,6 @@ import Hero from '@/components/Hero';
 import { getMeta } from '@/lib/github';
 import { getPages } from '@/lib/notion';
 
-export const revalidate = 60;
-
 export default async function Home() {
   const metaData = getMeta();
   const blogData = getPages();
